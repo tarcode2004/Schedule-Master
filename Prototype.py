3a4dab6f959ec9a3e94733e56ef6332bc60d9205
tarcode2004 <66574422+tarcode2004@users.noreply.github.com>
2022-03-28 21:05:59 -0400

    Revert "Revert "Changes on 03/27/22""
    This reverts commit fbc311a1f2ce0b39bbc9d0758758c454f599ecc3.

--- Prototype.py
+++ Prototype.py
@@ -1,4 +1,5 @@
 from turtle import onclick, textinput
+from types import ClassMethodDescriptorType
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -7,23 +8,43 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
+from kivy.properties import StringProperty, BooleanProperty
 Window.size = (900, 720)
-class A(Screen):
+class Login(Screen):
+    bool = False
+    msg = StringProperty("")
+    Username = ""
     def Quit(self):
         exit()
-    def ActionI(self):
-        pass
+    def Confirm(self, un, pw):
+        if un in ["Christian", "Tarik", "Jhoan", "Cara"] and pw == "password":
+            self.msg = "yeah boi"
+            self.bool = True
+            self.Username = un
+            Menu.Username = un[0]
+        else:
+            self.msg = "Invalid Credentials" 
     def ActionII(self):
         pass
     def AddTask(self):
         pass
         
-class B(Screen):
+class Menu(Screen):
+    TaskProg = 50
+    Task = "Cardio"
+    NTask = "Clean Room"
+    Username = "Group 1"
+    
+    
+class Tasks(Screen):
     pass
-class C(Screen):
+class Social(Screen):
+    pass
+class Mile_Stone(Screen):
     pass
 class Control(ScreenManager):
     pass
+    
 kv = Builder.load_file('proto.kv')
 class ProtoApp(App):
     def build(self):
